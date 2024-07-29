30a5e0023ca640efc80abe9ad193fea20e0a871a
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2024-07-29 17:40:16 +0530

    Added Server Tests

---
+++ Flask-Server/tests/testconnection.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+TRU=`cat trueInput.txt`
+FAL=`cat falseInput.txt`
+
+echo "Output Should be True"
+curl -X POST -H "Content-Type: application/json" -d "{\"text\": \"$TRU\", \"model\": \"LR\"}" http://127.0.0.1:5000/predict
+
+echo
+echo "#############################"
+echo "#############################"
+echo
+
+echo "Output Should be False"
+curl -X POST -H "Content-Type: application/json" -d "{\"text\": \"$FAL\", \"model\": \"LR\"}" http://127.0.0.1:5000/predict
