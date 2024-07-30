af9f0e4cf04932dfacb10b6b4c653326b8a1671a
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2024-07-30 16:28:35 +0530

    Updates

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
         <div className="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
           <div className="flex flex-col w-full lg:w-1/3 justify-center items-start p-8">
             <h1 className="text-3xl md:text-5xl p-2 text-yellow-300 tracking-loose">
-              TechFest
+              NewsWave
             </h1>
             <h2 className="text-3xl md:text-5xl leading-relaxed md:leading-snug mb-2">
               Space : The Timeless Infinity
