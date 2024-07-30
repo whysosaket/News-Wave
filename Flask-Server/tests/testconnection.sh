28a0c0a7d0d5ec44064abc0f624c046962314a6c
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2024-07-30 14:09:06 +0530

    Docker Flask

--- Flask-Server/tests/testconnection.sh
+++ Flask-Server/tests/testconnection.sh
@@ -4,7 +4,7 @@ TRU=`cat trueInput.txt`
 FAL=`cat falseInput.txt`
 
 echo "Output Should be True"
-curl -X POST -H "Content-Type: application/json" -d "{\"text\": \"$TRU\", \"model\": \"LR\"}" http://127.0.0.1:5000/predict
+curl -X POST -H "Content-Type: application/json" -d "{\"text\": \"$TRU\", \"model\": \"LR\"}" http://127.0.0.1:8080/predict
 
 echo
 echo "#############################"
@@ -12,4 +12,4 @@ echo "#############################"
 echo
 
 echo "Output Should be False"
-curl -X POST -H "Content-Type: application/json" -d "{\"text\": \"$FAL\", \"model\": \"LR\"}" http://127.0.0.1:5000/predict
+curl -X POST -H "Content-Type: application/json" -d "{\"text\": \"$FAL\", \"model\": \"LR\"}" http://127.0.0.1:8080/predict
