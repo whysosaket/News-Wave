104db2f55ecd4c7e63775e3d6b832d447a88017c
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2024-07-30 16:12:25 +0530

    Added News Route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import News from "./pages/News";
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Login />} />
+        <Route path="/news" element={<News />} />
       </Routes>
       </Router>
     </>
