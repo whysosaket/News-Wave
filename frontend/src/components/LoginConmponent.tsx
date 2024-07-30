43d185dc02993982b04b3764ee6e346f005c215a
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2024-07-30 16:04:16 +0530

    Fix

--- frontend/src/components/LoginConmponent.tsx
+++ frontend/src/components/LoginConmponent.tsx
@@ -1,15 +1,9 @@
-
-
 const LoginComponent = () => {
- 
-
   return (
     <div>
       <div className="relative grid bg-black">
         <div className="flex flex-col sm:flex-row items-center md:items-start sm:justify-center md:justify-start flex-auto min-w-0">
-          <div
-            className="relative sm:w-1/2 xl:w-3/5 bg-transparent h-full hidden md:flex flex-auto items-center justify-center p-10 overflow-hidden text-white bg-no-repeat bg-cover relative"
-          >
+          <div className="sm:w-1/2 xl:w-3/5 bg-transparent h-full hidden md:flex flex-auto items-center justify-center p-10 overflow-hidden text-white bg-no-repeat bg-cover relative">
             <img className="h-96" src="login.svg" />
           </div>
 
@@ -18,14 +12,21 @@ const LoginComponent = () => {
               <div className="lg:text-left text-center">
                 <div className="flex items-center justify-center">
                   <div className="bg-black flex flex-col w-80 border border-gray-900 rounded-lg px-8 py-10">
-                    <form className="flex flex-col space-y-8 mt-10" onSubmit={()=> {}}>
-                      <label className="font-bold text-lg text-white">Email</label>
+                    <form
+                      className="flex flex-col space-y-8 mt-10"
+                      onSubmit={() => {}}
+                    >
+                      <label className="font-bold text-lg text-white">
+                        Email
+                      </label>
                       <input
                         type="email"
                         placeholder="Email"
                         className="border rounded-lg py-3 px-3 mt-4 bg-black border-indigo-600 placeholder-white text-white"
                       />
-                      <label className="font-bold text-lg text-white">Password</label>
+                      <label className="font-bold text-lg text-white">
+                        Password
+                      </label>
                       <input
                         type="password"
                         placeholder="****"
