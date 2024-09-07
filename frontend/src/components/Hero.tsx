a5f37716b74e50d84a62ebbe0ede70fe07b3b92f
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2024-09-07 14:31:12 +0530

    Changes

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -17,16 +17,15 @@ const Hero = () => {
             initial={{x: -200, opacity: 0}}
             animate={{x: 0, opacity: 1}}
             transition={{delay: 0.2, duration: 0.4}}
-            className="font-inter text-3xl md:text-5xl leading-relaxed md:leading-snug mb-2">
-              Space : The Timeless Infinity
+            className="font-inter text-3xl md:text-4xl leading-relaxed md:leading-snug mb-2">
+              The Pulse of Truth in a Sea of Stories
             </motion.h2>
             <motion.p
             initial={{x: -200, opacity: 0}}
             animate={{x: 0, opacity: 1}}
             transition={{delay: 0.3, duration: 0.4}}
             className="text-sm md:text-base text-gray-50 mb-4">
-              Explore your favourite events and register now to showcase your
-              talent and win exciting prizes.
+              Explore the world’s most important events, verified by cutting-edge technology. Stay ahead, stay informed, and be part of the wave.
             </motion.p>
             <motion.a
               initial={{x: -200, opacity: 0}}
@@ -35,7 +34,7 @@ const Hero = () => {
               href="#"
               className="bg-transparent hover:bg-yellow-300 text-yellow-300 hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border border-yellow-300 hover:border-transparent"
             >
-              Explore Now
+              Join the Movement
             </motion.a>
           </div>
           <div className="p-8 mt-12 mb-6 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3  justify-center">
